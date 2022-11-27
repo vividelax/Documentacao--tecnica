1018814890d1c55ac7ca3d35a4feba34aae884ce
vividelax <115476584+vividelax@users.noreply.github.com>
2022-11-27 15:06:38 -0300

    Create Escolha.js

---
+++ Escolha.js
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <div id="divprincipal">
+        <h1 style="margin-left: 150px">
+        VL - Escolha seu local
+        </h1>
+        <img src="https://mega.ibxk.com.br/2022/05/12/12181905792528.jpg?ims=1200x480" id="imagem">
+        <div style="display: flex; margin-left: 40px">
+        <p>
+            Local de Saída : 
+        </p>
+            <input class="inputs" style="margin-top: 6px; margin-left: 18px" placeholder="Saída">
+        </div>
+        <div style="display: flex; margin-left: 40px">
+        <p>
+            Local de destino : 
+        </p>
+            <input class="inputs" style="margin-top: 6px; margin-left: 10px" placeholder="Destino">
+        </div>
+        <div id="divprincipal2">
+            <div style="display: flex">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgdGH8x_1r4ZlzcZFUGDbb0taTtW8XMswLlg&usqp=CAU" id="imagem2" style="margin-top: 10px">
+                <p style="margin-top: 25px; margin-left: 20px;">
+                     VL Padrão
+                </p>
+                <p style="margin-top: 25px; margin-left: 200px;">
+                    R$ 10,00
+                </p>
+                <button class="botao" style="margin-top: 18px; margin-left: 14px">
+                    Selecionar
+                </button>
+            </div>
+            <div style="display: flex">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgdGH8x_1r4ZlzcZFUGDbb0taTtW8XMswLlg&usqp=CAU" id="imagem2" style="margin-top: 10px">
+                <p style="margin-top: 25px; margin-left: 20px;">
+                    VL Luxo
+                </p>
+                <p style="margin-top: 25px; margin-left: 220px;">
+                    R$ 17,00
+                </p>
+                <button class="botao" style="margin-top: 18px; margin-left: 14px">
+                    Selecionar
+                </button>
+            </div>
+        
+        </div>
+        <button id="confirmar" style="margin-left: 40px; margin-top: 20px;">
+            Confirmar Carona
+        </button>
+    </div>
+</body>
+</html>
